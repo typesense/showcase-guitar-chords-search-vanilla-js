c1fbd7e5880fa35312f64a09fb5c0b52e85c2b5b
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-20 19:46:22 +0700

    styling, search refinements & infiniteHits

--- main.js
+++ main.js
@@ -1,7 +1,77 @@
 import './style.css';
+import './styles/heading.css';
+import './styles/searchAndFilter.css';
+import './styles/infiniteHits.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Search guitar chords</h1>
-  </div>
-`;
+import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+import instantsearch from 'instantsearch.js';
+import {
+  configure,
+  infiniteHits,
+  refinementList,
+} from 'instantsearch.js/es/widgets';
+
+const adapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: import.meta.env.VITE_TYPESENSE_SEARCH_ONLY || 'xyz',
+    nodes: [
+      {
+        host: import.meta.env.VITE_TYPESENSE_HOST || 'localhost',
+        port: parseInt(import.meta.env.VITE_TYPESENSE_PORT || '8108'),
+        protocol: import.meta.env.VITE_TYPESENSE_PROTOCOL || 'http',
+      },
+    ],
+  },
+  additionalSearchParameters: {
+    query_by: 'key,suffix',
+    num_typos: 0,
+  },
+});
+
+const search = instantsearch({
+  indexName: 'guitar-chords',
+  searchClient: adapter.searchClient,
+  future: {
+    preserveSharedStateOnUnmount: true,
+  },
+});
+
+search.addWidgets([
+  configure({
+    hitsPerPage: 12,
+    enablePersonalization: true,
+  }),
+  infiniteHits({
+    container: '#infiniteHits',
+    templates: {
+      item(hit, { html, components }) {
+        return html`
+          <h2>
+            ${components.Highlight({ attribute: 'key', hit })}${hit.suffix}
+          </h2>
+        `;
+      },
+    },
+  }),
+  // Refinement lists
+  refinementList({
+    container: '#key_refinementList',
+    attribute: 'key',
+    sortBy: ['name'],
+  }),
+  refinementList({
+    container: '#suffix_refinementList',
+    attribute: 'suffix',
+    limit: 8,
+    showMore: true,
+    showMoreLimit: 100,
+    searchable: true,
+    searchablePlaceholder: 'Search suffixes...',
+  }),
+  refinementList({
+    container: '#capo_refinementList',
+    attribute: 'positions.capo',
+  }),
+]);
+
+search.start();
