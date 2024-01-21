16ad122946f85fbe93202ac4c24a79c57aae2d14
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-21 09:27:58 +0700

    e2e with cypress

---
+++ cypress/e2e/home.spec.cy.js
@@ -0,0 +1,17 @@
+describe('Visit website', () => {
+  const hitListClass = '.ais-InfiniteHits-list';
+
+  it('Load more button', () => {
+    cy.visit('/');
+    const showMoreBtn = cy.contains('Show more results');
+    cy.get(hitListClass).find('li').should('have.length', 12);
+    showMoreBtn.click();
+    cy.get(hitListClass).find('li').should('have.length', 24);
+  });
+
+  it('Display no result when search "noResultTestSuffixes"', () => {
+    cy.visit('/');
+    cy.get('[placeholder="Search suffixes..."]').type('noResultTestSuffixes');
+    cy.contains(/No results/);
+  });
+});
